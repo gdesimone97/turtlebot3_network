3ae3a54b97f21c3f349f577b654fecbaa92a7b80
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2022-05-10 16:41:10 +0200

    fix2

--- host_run.bash
+++ host_run.bash
@@ -2,4 +2,4 @@
 
 sleep 3
 source $HOME/turtlebot3_network/venv/bin/activate
-python3 $HOME/turtlebot3_network/host.py > /home/gdesimone/code/turtlebot/log_host.txt
+python3 $HOME/turtlebot3_network/host.py > $HOME/turtlebot3_network/log_host.txt
--- server_run.bash
+++ server_run.bash
@@ -2,4 +2,4 @@
 
 sleep 3
 source $HOME/turtlebot3_network/venv/bin/activate
-python3 $HOME/turtlebot3_network/server.py > /home/gdesimone/code/network/log.txt
+python3 $HOME/turtlebot3_network/server.py > $HOME/turtlebot3_network/log.txt
