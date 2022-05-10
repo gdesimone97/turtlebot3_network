150425dcff5d31a948a9ae6b213d8aa61402264d
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2022-05-10 17:19:11 +0200

    Update host.py

--- host.py
+++ host.py
@@ -1,16 +1,15 @@
 import subprocess
 import requests
-from utils import check_connetion, get_host_ip
+from utils import check_connetion, get_host_ip, get_curr_dir, get_interface
 from pathlib import Path
 import os
 
 if __name__ == "__main__":
-    check_connetion()
-    ip = get_host_ip()
+    device = get_interface()
+    ip = get_host_ip(device)
     exe_path = os.path.abspath(os.path.dirname(os.path.relpath(__file__)))
     exe_path = Path(exe_path).joinpath("modify.py")
     cmd = f"python3 {exe_path} {ip} {ip}"
     cmd = cmd.split(" ")
     r = subprocess.run(cmd)
     print("Return code:", r.returncode)
-
