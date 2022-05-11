1b729e2870704c18f655ae53d3dec80cac997a76
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2022-05-11 10:31:47 +0200

    Update client.py

--- client.py
+++ client.py
@@ -4,6 +4,6 @@ import requests
 # requests.get("http://192.138.1.1", timeout=0.1)
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientSocket.connect(("127.0.0.1",9090))
+clientSocket.connect(("server-peppodesmo.ddns.net",9090))
 data = "192.168.1.1"
 clientSocket.send(data.encode())
