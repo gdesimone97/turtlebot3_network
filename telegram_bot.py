2b75e7aa3e091d18577d560557077a71bc2c4f3e
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2022-05-11 10:16:00 +0200

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,7 +1,5 @@
 from telegram.ext import Updater, CommandHandler, CallbackContext
 from telegram import Update
-import pandas as pd
-from pathlib import Path
 import os
 import socket
 from threading import Thread
@@ -10,7 +8,6 @@ from utils import get_host_ip, get_interface
 
 class Handler:
     def __init__(self, dispatcher, updater):
-        curr_dir = self._get_curr_dir()
         self.dispatcher = dispatcher
         self.updater = updater
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -18,13 +15,6 @@ class Handler:
         self.server_socket.bind((get_host_ip(device), 9090))
         self.server_socket.listen()
         self.live = True
-        self.df_columns = ["userid"]
-        self.df_path = Path(curr_dir).joinpath("users.csv")
-        if not self.df_path.exists():
-            self.df = pd.DataFrame(columns=self.df_columns)
-            self.save_df()
-        else:
-            self.df = pd.read_csv(self.df_path, index_col=0)
         self._ip = ""
         Thread(target=self.thread).start()
 
@@ -37,16 +27,6 @@ class Handler:
             context = CallbackContext(self.dispatcher)
             self.echo(context)
             time.sleep(0.5)
-
-    def save_df(self):
-        self.df.to_csv(self.df_path)
-
-    def start_callback(self, update: Update, context: CallbackContext):
-        userid = update.effective_user.id
-        data = {self.df_columns[0]: [userid]}
-        self.df = pd.concat([self.df, pd.DataFrame(data)])
-        self.df = self.df.drop_duplicates()
-        self.save_df()
 
     def _get_curr_dir(self):
         curr_dir = os.path.abspath(os.path.dirname(os.path.relpath(__file__)))
