d6bc05e3f404ebfd271f7d9aacf8970b2e0540de
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2022-06-29 09:01:35 +0200

    Update client.py

--- client.py
+++ client.py
@@ -3,7 +3,9 @@ import requests
 
 # requests.get("http://192.138.1.1", timeout=0.1)
 
+TELEGRAM_SERVER = "server-peppodesmo.ddns.net"
+# TELEGRAM_SERVER = "193.205.163.163"
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientSocket.connect(("193.205.163.163",9090))
+clientSocket.connect((TELEGRAM_SERVER, 9090))
 data = "192.168.1.1"
 clientSocket.send(data.encode())
