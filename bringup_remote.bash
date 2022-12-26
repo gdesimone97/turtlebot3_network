94372a51f54fcf0794e5ed4a7e693191915c1e10
Giuseppe De Simone <44608428+gdesimone97@users.noreply.github.com>
2022-12-26 17:39:47 +0100

    Update bringup_remote.bash

--- bringup_remote.bash
+++ bringup_remote.bash
@@ -2,4 +2,3 @@
 
 sshpass -p turtlebot ssh -o StrictHostKeyChecking=no ubuntu@$1 /bin/bash
 sshpass -p turtlebot ssh -o StrictHostKeyChecking=no ubuntu@$1 ps aux | grep turtlebot3_robot.launch | awk "{print $1}"
-echo "ciao"
