5db462957c0610553d0693f3bd06ee64b829a78d
Giuseppe De Simone <44608428+gdesimone97@users.noreply.github.com>
2022-12-26 17:39:53 +0100

    Delete bringup_remote.bash

--- bringup_remote.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sshpass -p turtlebot ssh -o StrictHostKeyChecking=no ubuntu@$1 /bin/bash
-sshpass -p turtlebot ssh -o StrictHostKeyChecking=no ubuntu@$1 ps aux | grep turtlebot3_robot.launch | awk "{print $1}"
