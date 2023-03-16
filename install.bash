b2f9283cabea1b7afb7cce988788e1b89a6e5ac9
Giuseppe De Simone <44608428+gdesimone97@users.noreply.github.com>
2023-03-16 15:34:17 +0100

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,6 +1,6 @@
 
 sudo apt update
-sudo apt install -y sshpass fping
+sudo apt install -y sshpass fping python3-pip
 python3 -m pip install -r requirements.txt
 sudo chmod +x get_ip_wlan
 sudo cp get_ip_wlan /usr/local/bin
