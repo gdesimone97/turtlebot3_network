cd83cabbbc689d1f36dcd82c7b1b857bd07bb4e9
Giuseppe De Simone <44608428+gdesimone97@users.noreply.github.com>
2023-03-17 10:48:53 +0100

    Update install.bash

--- install.bash
+++ install.bash
@@ -10,6 +10,7 @@ sudo cp get_ip_wlan /usr/local/bin
 export INTERFACE=$1
 USR=$(whoami)
 source /home/$USR/.bashrc
+echo "export TURTLEBOT3_MODEL=waffle_pi" >> /home/$USR/.bashrc
 echo "export INTERFACE=$INTERFACE" >> /home/$USR/.bashrc
 echo "IP=$(get_ip_wlan)" >> /home/$USR/.bashrc
 echo "export ROS_MASTER_URI=http://\$IP:11311/" >> /home/$USR/.bashrc
