9146d84b0770909bda78a501866186fac9a6ef9d
Giuseppe De Simone <44608428+gdesimone97@users.noreply.github.com>
2023-03-17 10:12:54 +0100

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,12 +1,16 @@
-
+if [ "$#" -ne 1 ]; then
+	echo "Illegal number of parameters"
+	exit
+fi
 sudo apt update
 sudo apt install -y sshpass fping python3-pip
 python3 -m pip install -r requirements.txt
 sudo chmod +x get_ip_wlan
 sudo cp get_ip_wlan /usr/local/bin
-INTERFACE=$1
+export INTERFACE=$1
 USR=$(whoami)
+source /home/$USR/.bashrc
 echo "export INTERFACE=$INTERFACE" >> /home/$USR/.bashrc
 echo "IP=$(get_ip_wlan)" >> /home/$USR/.bashrc
-echo "export ROS_MASTER_URI=http://$IP:11311/" >> /home/$USR/.bashrc
-echo "export ROS_HOSTNAME=$IP" >> /home/$USR/.bashrc
+echo "export ROS_MASTER_URI=http://\$IP:11311/" >> /home/$USR/.bashrc
+echo "export ROS_HOSTNAME=\$IP" >> /home/$USR/.bashrc
