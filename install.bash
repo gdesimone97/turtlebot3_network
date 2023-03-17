7e43a4a00280e19ac856510673fabbdbe2aca93f
Giuseppe De Simone <44608428+gdesimone97@users.noreply.github.com>
2023-03-17 09:51:29 +0100

    Update install.bash

--- install.bash
+++ install.bash
@@ -7,3 +7,6 @@ sudo cp get_ip_wlan /usr/local/bin
 INTERFACE=$1
 USR=$(whoami)
 echo "export INTERFACE=$INTERFACE" >> /home/$USR/.bashrc
+IP=$(get_ip_wlan)
+echo "export ROS_MASTER_URI=http://$IP:11311/" >> /home/$USR/.bashrc
+echo "export ROS_HOSTNAME=$IP" >> /home/$USR/.bashrc
